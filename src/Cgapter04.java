3984468fe9f96ec883e0e5bce2d6a0e453d6ec76
sgmt1001 <sgmt1001@gmail.com>
2021-10-07 13:39:22 +0900

    main

---
+++ src/Cgapter04.java
@@ -0,0 +1,12 @@
+
+public class Cgapter04 {
+    public static void main(String[] args) {
+        // 定数の宣言
+        final int LUCKY_NUMBER = 666;
+        String str = "重本敏貴";
+        System.out.println(str);
+        str = "You Only Live Once";
+        System.out.println(str); 
+        System.out.println(LUCKY_NUMBER);
+    }
+}
